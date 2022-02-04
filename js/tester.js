cfa6c424c8a4d48c3728479a295477d70a47cb05
Raheem Shah <Raheemshah2809@gmail.com>
2022-02-04 17:20:23 +0000

    Added Search bar

--- js/tester.js
+++ js/tester.js
@@ -1,7 +1,6 @@
 const body = document.querySelector('body'),
             sidebar = body.querySelector('nav'),
             toggle = body.querySelector(".toggle"),
-            searchBtn = body.querySelector(".search-box"),
             modeSwitch = body.querySelector(".toggle-switch"),
             modeText = body.querySelector(".mode-text");
 
@@ -9,11 +8,7 @@ const body = document.querySelector('body'),
         toggle.addEventListener("click", () => {
             sidebar.classList.toggle("close");
         })
-
-        searchBtn.addEventListener("click", () => {
-            sidebar.classList.remove("close");
-        })
-
+        
         modeSwitch.addEventListener("click", () => {
             body.classList.toggle("dark");
 
